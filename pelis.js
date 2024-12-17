aad40e68acc05f362b65a1ccd44c435ebb060ec1
hernest <ernestoami@hotmail.com>
2024-12-17 10:26:29 +0100

    new url

--- pelis.js
+++ pelis.js
@@ -1,5 +1,5 @@
 
-const URL = 'https://github.com/herneshem/herneshem.github.io/blob/master/movies-250.json';
+const URL = 'https://herneshem.github.io/movies-250.json';
 
 function processMovie(data) {
     console.log(data);
